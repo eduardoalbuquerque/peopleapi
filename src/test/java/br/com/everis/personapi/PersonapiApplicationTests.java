d888ae025f39ddcedd7ac37d08745074d397ea59
Eduardo Albuquerque <eduardo.albuquerque@hotmail.com.br>
2020-12-10 19:51:03 -0300

    start the project - first commit, structure base

---
+++ src/test/java/br/com/everis/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.everis.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
